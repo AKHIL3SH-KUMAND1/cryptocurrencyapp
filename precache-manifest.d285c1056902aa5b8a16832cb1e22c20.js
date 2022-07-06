864bbb0249ca5e1495c8c12c4303dc479f34627f
AKHIL3SH-KUMAND1 <96103942+AKHIL3SH-KUMAND1@users.noreply.github.com>
2022-07-06 07:01:05 +0530

    Updates

---
+++ precache-manifest.d285c1056902aa5b8a16832cb1e22c20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87e4e5771ac95cea0ea",
+    "url": "/crypto-application/static/css/main.e23ed0ea.chunk.css"
+  },
+  {
+    "revision": "d87e4e5771ac95cea0ea",
+    "url": "/crypto-application/static/js/main.d87e4e57.chunk.js"
+  },
+  {
+    "revision": "90ecf6b5710b2396cef7",
+    "url": "/crypto-application/static/js/1.90ecf6b5.chunk.js"
+  },
+  {
+    "revision": "5adda5bf9f213b8ce213",
+    "url": "/crypto-application/static/css/2.58bc571b.chunk.css"
+  },
+  {
+    "revision": "5adda5bf9f213b8ce213",
+    "url": "/crypto-application/static/js/2.5adda5bf.chunk.js"
+  },
+  {
+    "revision": "237144b4ebc0868604cb",
+    "url": "/crypto-application/static/js/runtime~main.237144b4.js"
+  },
+  {
+    "revision": "af7a7720ac483f616ce70cef7d1afaf9",
+    "url": "/crypto-application/index.html"
+  }
+];
